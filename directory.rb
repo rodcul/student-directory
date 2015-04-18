7356b81f955e4e3812b99e249849b24f3bd94254
Rodney Cullen <rodcul@gmail.com>
2015-04-18 15:43:41 +0100

    ex6

--- directory.rb
+++ directory.rb
@@ -4,15 +4,26 @@ def input_students
 	# create an empty array
 	students = []
 	# get the first name
+	puts "Name"
 	name = gets.chomp
+	puts "\nCohort"
+	cohort = gets.chomp
+	puts "\nCountry"
+	country = gets.chomp
 
 	# while the name is not empty, repeat this code
 while !name.empty? do
 	#add the student hash to the array
-	students << {:name => name, :cohort => :november}
+	students << {:name => name, :cohort => cohort, :country => country}
 	puts "Now we have #{students.length} students"
 	# get another name from the user
+	puts "Name"
 	name = gets.chomp
+	return students if name.empty?
+	puts "\nCohort"
+	cohort = gets.chomp
+	puts "\nCountry"
+	country = gets.chomp
 end
 
 #return the array of students
@@ -27,7 +38,7 @@ end
 
 def print(students)
   students.each_with_index() do |student,index|
-     puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)" if student[:name].chars.first == "A" && student[:name].length < 12
+     puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort from #{student[:country]})"
 
   end
 end
@@ -35,6 +46,8 @@ end
 def print_footer(names)
   puts "\nOverall, we have #{names.length} great students"
 end 
+
+
 #nothing happens until we call the methods
 students = input_students
 print_header
