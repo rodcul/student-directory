a520e22135fd58b98970cd4311557534da5b10a7
Rodney Cullen <rodcul@gmail.com>
2015-04-18 21:24:28 +0100

    Version 9: Loading the data from the file

--- directory.rb
+++ directory.rb
@@ -18,6 +18,7 @@ def print_menu
 	puts "1. Input the students"
 	puts "2. Show the students"
 	puts "3. Save the list to students.csv"
+	puts "4. Load the students from students.csv"
 	puts "9. Exit"
 
 end
@@ -28,8 +29,10 @@ def process(selection)
 	        input_students
 	      when "2"
 	        show_students
-	    when "3"
+	      when "3"
 	    	save_students
+	      when "4"
+	      	load_students
 	      when "9"
 	        exit # this will cause the program to terminate
 	      else
@@ -92,6 +95,16 @@ def save_students
 		file.puts csv_line
 	end
 	file.close	
+end
+
+def load_students
+	file = File.open("students.csv", "r")
+	file.readlines.each do |line|
+		name, cohort, country = line.chomp.split(',')
+		@students << {:name => name, :cohort => cohort.to_sym, :country => country}
+	end
+	file.close
+	
 end
 
 
